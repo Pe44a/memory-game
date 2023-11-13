ac22158ae59c0f905ba5466b49f4c4783e4af138
Peteris <blankfeldspeteris@gmail.com>
2023-11-13 20:42:17 +0200

    Styled cards

--- memory-game/src/assets/cardsRender.jsx
+++ memory-game/src/assets/cardsRender.jsx
@@ -8,10 +8,23 @@ function Cards() {
     const [score, setScore] = useState(0);
     // const [highestScore, setHighestScore] = useState(0);
 
-    const cardsArray = [{cardName:'bart', imageURL:'./public/card-images/bart.png', clicked:false,  id:uuidv4()}];
+    const cardsArray = [
+        {cardName:'Bart', imageURL:'./card-images/bart.png', clicked:false,  id:uuidv4()},
+        {cardName:'Homer', imageURL:'./card-images/homer.png', clicked:false,  id:uuidv4()},
+        {cardName:'Lisa', imageURL:'./card-images/lisa.png', clicked:false,  id:uuidv4()},
+        {cardName:'Maggie', imageURL:'./card-images/maggie.png', clicked:false,  id:uuidv4()},
+        {cardName:'Marge', imageURL:'./card-images/marge.png', clicked:false,  id:uuidv4()},
+        {cardName:'Milhouse', imageURL:'./card-images/milhouse.png', clicked:false,  id:uuidv4()},
+        {cardName:'Mr.Burns', imageURL:'./card-images/mr._burns.png', clicked:false,  id:uuidv4()},
+        {cardName:'Ned Flanders', imageURL:'./card-images/ned_flanders.png', clicked:false,  id:uuidv4()},
+        {cardName:'Skinner', imageURL:'./card-images/skinner.png', clicked:false,  id:uuidv4()},
+        {cardName:'Smithers', imageURL:'./card-images/smithers.png', clicked:false,  id:uuidv4()},
+        {cardName:'Willie', imageURL:'./card-images/willie.png', clicked:false,  id:uuidv4()}
+    ];
 
 
     function updatesScore(info) {
+
         if(info.clicked === true) {
 
             let scoreDuplicate = {score};
