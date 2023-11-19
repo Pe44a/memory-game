69f8a08416a9652ad2fa79e8cbe4e6a5bb52e45e
Peteris <blankfeldspeteris@gmail.com>
2023-11-19 22:59:49 +0200

    Counts score & restarts score
    -Restarts score if card is clicked twice.

--- memory-game/src/assets/cardsRender.jsx
+++ memory-game/src/assets/cardsRender.jsx
@@ -4,44 +4,60 @@ import { v4 as uuidv4 } from "uuid";
 import Card from "./card.jsx";
 
 function Cards() {
-
-    const [score, setScore] = useState(0);
-    // const [highestScore, setHighestScore] = useState(0);
-
     const cardsArray = [
-        {cardName:'Bart', imageURL:'./card-images/bart.png', clicked:false,  id:uuidv4()},
-        {cardName:'Homer', imageURL:'./card-images/homer.png', clicked:false,  id:uuidv4()},
-        {cardName:'Lisa', imageURL:'./card-images/lisa.png', clicked:false,  id:uuidv4()},
-        {cardName:'Maggie', imageURL:'./card-images/maggie.png', clicked:false,  id:uuidv4()},
-        {cardName:'Marge', imageURL:'./card-images/marge.png', clicked:false,  id:uuidv4()},
-        {cardName:'Milhouse', imageURL:'./card-images/milhouse.png', clicked:false,  id:uuidv4()},
-        {cardName:'Mr.Burns', imageURL:'./card-images/mr._burns.png', clicked:false,  id:uuidv4()},
-        {cardName:'Ned Flanders', imageURL:'./card-images/ned_flanders.png', clicked:false,  id:uuidv4()},
-        {cardName:'Skinner', imageURL:'./card-images/skinner.png', clicked:false,  id:uuidv4()},
-        {cardName:'Smithers', imageURL:'./card-images/smithers.png', clicked:false,  id:uuidv4()},
-        {cardName:'Willie', imageURL:'./card-images/willie.png', clicked:false,  id:uuidv4()}
+        {cardName:'Bart', imageURL:'./card-images/bart.png', clicked:false, id:uuidv4()},
+        {cardName:'Homer', imageURL:'./card-images/homer.png', clicked:false, id:uuidv4()},
+        {cardName:'Lisa', imageURL:'./card-images/lisa.png', clicked:false, id:uuidv4()},
+        {cardName:'Maggie', imageURL:'./card-images/maggie.png', clicked:false, id:uuidv4()},
+        {cardName:'Marge', imageURL:'./card-images/marge.png', clicked:false, id:uuidv4()},
+        {cardName:'Milhouse', imageURL:'./card-images/milhouse.png', clicked:false, id:uuidv4()},
+        {cardName:'Mr.Burns', imageURL:'./card-images/mr._burns.png', clicked:false, id:uuidv4()},
+        {cardName:'Ned Flanders', imageURL:'./card-images/ned_flanders.png', clicked:false, id:uuidv4()},
+        {cardName:'Skinner', imageURL:'./card-images/skinner.png', clicked:false, id:uuidv4()},
+        {cardName:'Smithers', imageURL:'./card-images/smithers.png', clicked:false, id:uuidv4()},
+        {cardName:'Willie', imageURL:'./card-images/willie.png', clicked:false, id:uuidv4()}
     ];
 
 
-    function updatesScore(info) {
+    const [score, setScore] = useState(0);
+    const [array, setArray] = useState(cardsArray)
+    // const [highestScore, setHighestScore] = useState(0);
 
-        if(info.clicked === true) {
 
-            let scoreDuplicate = {score};
-            scoreDuplicate += 1;
-            setScore(scoreDuplicate)
+    function updatesScore(index) {
+        const updatedCardsArray = array;
+
+        if(updatedCardsArray[index].clicked === true) {
+
+            // resets points
+            setScore(0)
+
+            // resets cardsArrays clicked values to false
+            updatedCardsArray.map((cardArray) => {
+                cardArray.clicked = false;
+            });
+            setArray(updatedCardsArray)
         } else {
+            
+            // adds point to score
+            let scoreClone = score;
+            scoreClone += 1;
+            setScore(scoreClone)
 
-            info.clicked = true;
+            updatedCardsArray[index].clicked = true;
+            setArray(updatedCardsArray);
         }
     }
 
     return(
         <>
+        <div>Sore: {score}</div>
         <div className="cards">
-            {cardsArray.map((card) => (
+
+            {cardsArray.map((card, index) => (
+
                 <Card key={card.id} cardName={card.cardName} imageURL={card.imageURL}
-                onClickFunction={()=> {updatesScore(card)}}/>
+                onClickFunction={()=> {updatesScore(index)}}/>
             ))}
         </div>
         </>
